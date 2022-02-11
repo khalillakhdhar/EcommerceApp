f5fd9f7bb46c7a3556841ce0a002002979adf2cf
Oussama814 <oussama.tarkhani97@gmail.com>
2022-02-11 14:48:28 +0100

    produit

--- src/main/java/com/sec/model/ventes/Produits.java
+++ src/main/java/com/sec/model/ventes/Produits.java
@@ -1,5 +1,42 @@
 package com.sec.model.ventes;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Produits {
+	
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id ;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "titre", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonProperty("titre")
+	private Categorie cat;
+	
+	@Column(nullable = false)
+	private String photo ;
+	@Column(nullable = false)
+	
+	private String description ;
+	@Column(nullable = false)
+	private String categorie ;
+	
+	@Column(nullable = false)
+	private double prix;
+	
+	@Column(name = "id_commande")
+	private long idCommande;
 
 }
