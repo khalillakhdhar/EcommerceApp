37cc30fa2203d578efe87b215e2e7700035aab7b
khalil <khalillakhdharatc@gmail.com>
2022-02-11 15:28:22 +0100

    Update User.java

--- src/main/java/com/sec/model/users/User.java
+++ src/main/java/com/sec/model/users/User.java
@@ -1,11 +1,13 @@
 package com.sec.model.users;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@Entity
 public class User {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
