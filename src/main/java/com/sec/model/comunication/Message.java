b16c9fcfaaa59f3412a29b5da4553e20c45c799b
Oussama814 <oussama.tarkhani97@gmail.com>
2022-02-11 15:01:25 +0100

    message

--- src/main/java/com/sec/model/comunication/Message.java
+++ src/main/java/com/sec/model/comunication/Message.java
@@ -1,5 +1,68 @@
 package com.sec.model.comunication;
 
-public class Message {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Message {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+	
+	@Column(nullable = false)
+    private String texte;
+	
+	@Column(nullable = false)
+    private Date dateheure;
+
+	@Column(nullable = false)
+    private String type;
+
+	public Message(Long id, String texte, Date dateheure, String type) {
+		super();
+		this.id = id;
+		this.texte = texte;
+		this.dateheure = dateheure;
+		this.type = type;
+	}
+
+	public Message() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexte() {
+		return texte;
+	}
+
+	public void setTexte(String texte) {
+		this.texte = texte;
+	}
+
+	public Date getDateheure() {
+		return dateheure;
+	}
+
+	public void setDateheure(Date dateheure) {
+		this.dateheure = dateheure;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 }
