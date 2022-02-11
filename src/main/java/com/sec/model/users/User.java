ed008ee80576aa9e78c06a9d3b19ab432b34fe5c
khalil <khalillakhdharatc@gmail.com>
2022-02-11 15:23:30 +0100

    Update User.java

--- src/main/java/com/sec/model/users/User.java
+++ src/main/java/com/sec/model/users/User.java
@@ -1,5 +1,15 @@
 package com.sec.model.users;
 
+<<<<<<< Updated upstream
+=======
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+
+>>>>>>> Stashed changes
 public class User {
 
 }
