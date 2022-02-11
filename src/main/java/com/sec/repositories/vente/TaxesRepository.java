2e8f3c685e7df9c50265647c361d105abae72bbd
Oussama814 <oussama.tarkhani97@gmail.com>
2022-02-11 16:12:06 +0100

    repo

---
+++ src/main/java/com/sec/repositories/vente/TaxesRepository.java
@@ -0,0 +1,9 @@
+package com.sec.repositories.vente;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.sec.model.ventes.Taxes;
+
+public interface TaxesRepository extends CrudRepository<Taxes, Long> {
+
+}
