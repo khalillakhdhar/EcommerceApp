7c11d60d23dfe3ba7b9a794e48577d7e2e881dee
Sourour814 <alouisourour814@gmail.com>
2022-02-11 14:50:58 +0100

    classe user

--- src/main/java/com/sec/model/users/User.java
+++ src/main/java/com/sec/model/users/User.java
@@ -1,6 +1,95 @@
 package com.sec.model.users;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 public class User {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+	@NotBlank
+     private String nom;
+	@NotBlank
+    private String prenom;
+
+	@NotBlank
+    @Email
+    private String email;
+	@NotBlank
+    private String password;
+	@NotBlank
+    private String age;
+	@NotBlank
+    private String adresse;
+	@NotBlank
+    private String telephone;
+	@NotBlank
+	private Grade grade;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public Grade getGrade() {
+		return grade;
+	}
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+    
+
+    
+
 	
 
 }
