7956cf567c6d90007632f9df4b10b6ad9cb1369c
khalil <khalillakhdharatc@gmail.com>
2022-02-11 15:22:11 +0100

    Update Produits.java

--- src/main/java/com/sec/model/ventes/Produits.java
+++ src/main/java/com/sec/model/ventes/Produits.java
@@ -1,5 +1,109 @@
 package com.sec.model.ventes;
 
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Produits {
+
+	
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id ;
+	
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "titre", nullable = false)
+	
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonProperty("titre")
+	private Categorie cat;
+	
+	@NotBlank
+	private String photo ;
+	@Column(nullable = false)
+	
+	private String description ;
+	@NotBlank
+	private String categorie ;
+	
+	@NotBlank
+	private double prix;
+	
+	
+	private long idCommande;
+
+	public Produits() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Categorie getCat() {
+		return cat;
+	}
+
+	public void setCat(Categorie cat) {
+		this.cat = cat;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+	public long getIdCommande() {
+		return idCommande;
+	}
+
+	public void setIdCommande(long idCommande) {
+		this.idCommande = idCommande;
+	}
+	
+	
+
 
 }
