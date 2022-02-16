d8d8a6451a238ec444f58f23d948e9b66ee0e454
Oussama814 <oussama.tarkhani97@gmail.com>
2022-02-16 10:43:43 +0100

    test

--- src/test/java/com/sec/globale/TestGlobale.java
+++ src/test/java/com/sec/globale/TestGlobale.java
@@ -1,5 +1,10 @@
 package com.sec.globale;
 
 public class TestGlobale {
+	public boolean verifNonNull(String ch)
+	{
+		return ch.isEmpty();
+	}
+	
 
 }
--- src/test/java/com/sec/particulier/UserTestImplement.java
+++ src/test/java/com/sec/particulier/UserTestImplement.java
@@ -7,25 +7,94 @@ public class UserTestImplement implements UserTestInterface {
 	@Override
 	public boolean mailValidation(String email) {
 		// TODO Auto-generated method stub
-		return false;
+		if(email.length()<10)
+			return false;
+		else  {
+			return (email.contains("@")&&email.lastIndexOf("@")<email.length()-2) && (email.contains(".")&&email.lastIndexOf(".")<email.length()-3);
+		}
+		
 	}
 
 	@Override
 	public boolean passValidation(String password) {
 		// TODO Auto-generated method stub
-		return false;
-	}
-
+		 
+        if (password.length() <8) {
+            return false;
+        }
+        
+        if (password.contains("@") || password.contains("#")){
+        	return false;
+        }
+        
+ 
+        
+        if (true) {
+            int count = 0;
+ 
+         
+            for (int i = 0; i <= 9; i++) {
+ 
+                
+                String str1 = Integer.toString(i);
+ 
+                if (password.contains(str1)) {
+                    count = 1;
+                }
+            }
+            if (count == 0) {
+                return false;
+            }
+        }
+        if (true) {
+            int count = 0;
+ 
+            
+            for (int i = 65; i <= 90; i++) {
+ 
+                
+                char c = (char)i;
+ 
+                String str1 = Character.toString(c);
+                if (password.contains(str1)) {
+                    count = 1;
+                }
+            }
+            if (count == 0) {
+                return false;
+            }
+        }
+        
+        if (true) {
+            int count = 0;
+ 
+          
+            for (int i = 90; i <= 122; i++) {
+ 
+                
+                char c = (char)i;
+                String str1 = Character.toString(c);
+ 
+                if (password.contains(str1)) {
+                    count = 1;
+                }
+            }
+            if (count == 0) {
+                return false;
+            }
+        }
+        return true;
+        }
 	@Override
 	public boolean telValidation(String telephone) {
-		// TODO Auto-generated method stub
-		return false;
+		return telephone.length()>8;
+		
 	}
 
 	@Override
 	public boolean ageValidation(int age) {
 		// TODO Auto-generated method stub
-		return false;
+		return age>0;
 	}
 
 }
