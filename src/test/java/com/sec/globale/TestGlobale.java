332a16c153f44d3d681d7def25df07a297445072
Oussama814 <oussama.tarkhani97@gmail.com>
2022-02-16 12:16:04 +0100

    test

--- src/test/java/com/sec/globale/TestGlobale.java
+++ src/test/java/com/sec/globale/TestGlobale.java
@@ -1,13 +1,10 @@
 package com.sec.globale;
 
 public class TestGlobale {
-public boolean verifStringNonNull(String ch)
-{
-return !ch.isEmpty();	
+	public boolean verifNonNull(String ch)
+	{
+		return !(ch.isEmpty());
+	}
+	
 
-}
-public boolean detectlength(int longueur,String ch)
-{
-return ch.length()>longueur;	
-}
 }
