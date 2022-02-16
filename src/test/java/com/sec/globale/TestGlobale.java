e875cbd2541397640f01a350c46465e104fdd1da
khalil <khalillakhdharatc@gmail.com>
2022-02-16 11:23:29 +0100

    maintest

--- src/test/java/com/sec/globale/TestGlobale.java
+++ src/test/java/com/sec/globale/TestGlobale.java
@@ -1,5 +1,13 @@
 package com.sec.globale;
 
 public class TestGlobale {
+public boolean verifStringNonNull(String ch)
+{
+return !ch.isEmpty();	
 
+}
+public boolean detectlength(int longueur,String ch)
+{
+return ch.length()>longueur;	
+}
 }
