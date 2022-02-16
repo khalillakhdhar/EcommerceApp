2966041f0681bda25f4b4b642e4531160184f886
khalil <khalillakhdharatc@gmail.com>
2022-02-16 10:03:29 +0100

    teste interfaces

---
+++ src/test/java/com/sec/particulier/testInterfaces/UserTestInterface.java
@@ -0,0 +1,8 @@
+package com.sec.particulier.testInterfaces;
+
+public interface UserTestInterface {
+boolean mailValidation(String email);
+boolean passValidation(String password);
+boolean telValidation(String telephone);
+boolean ageValidation(int age);
+}
